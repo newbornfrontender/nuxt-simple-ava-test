04aa4dc04a9f78ecb60f811e135634b6650e6a54
Infant Frontender <newbornfrontender@gmail.com>
2018-09-27 19:06:57 +0300

    Добавил конфигурацию

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+export default {
+  head: {
+    meta: [{
+      charset: 'utf-8',
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes',
+    }, {
+      'http-equiv': 'x-ua-compatible',
+      content: 'ie=edge',
+    }],
+  },
+
+  router: {
+    linkActiveClass: 'link-active',
+    linkExactActiveClass: 'link-exact',
+  },
+
+  build: {
+    postcss: {
+      preset: {
+        stage: 0,
+
+        browsers: 'last 2 versions',
+
+        autoprefixer: {
+          grid: process.env.NODE_ENV === 'development' ? false : true,
+        },
+      },
+    },
+  },
+
+  loading: {
+    color: 'green',
+  },
+};
